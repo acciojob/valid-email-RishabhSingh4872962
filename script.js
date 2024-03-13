8296aa2095f1505f2ebc77fd1b654a309d548287
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-03-13 10:35:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 function validEmail(str) {
   //your JS code here.
+	if (str==""|| str==" ") {
+		return false;
+	}
+	let emialDomain=[".com",".com.in",".co.in",".edu",".net",".org",".gov"];
+
+	emialDomain.forEach((dom)=>{
+		if (str.endsWith(dom)) {
+			for (let s of str) {
+				
+			}
+			
+		}
+	})
+	return false;
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
+// const str = prompt("Enter an email address.");
 alert(validEmail(str));
